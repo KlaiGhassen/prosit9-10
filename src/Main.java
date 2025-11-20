9222c73d7bdcc97ba57c9ce77d1781df08bf3389
KlaiGhassen <ghassen.klai@esprit.tn>
2025-11-20 16:39:17 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
-import java.util.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        Set<Person> students = new HashSet<>();
-        students.add(new Person(2, 21, "yassine"));
-        students.add(new Person(6, 23, "souha"));
-        students.add(new Person(1, 24, "Ahmed"));
-        students.add(new Person(2, 21, "hedi"));
-        students.add(new Person(3, 23, "nessrine"));
-        students.add(new Person(5, 22, "Ayoub"));
-        System.out.println(students);
-        System.out.println("--------------------------");
-        students = new TreeSet<>(students);
-        System.out.println(students);
-
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
